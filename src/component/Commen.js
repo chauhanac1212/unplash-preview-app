3355bc7731e0672ab837077f99a7842be2b95048
abhishek <abhishekkumar@appscrip.co>
2022-10-31 11:42:56 +0530

    add pagintion

--- src/component/Commen.js
+++ src/component/Commen.js
@@ -1,65 +1,118 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import './Commen.css'
 import Images from "./Images";
-import "./Commen.css";
-const Commen = () => {
+// import Pagination from '@material-ui/lab/Pagination';
+const  REACT_APP_KEY=`fNZxU3j0BwP87u2Fco6aFSZOkn-4S9kmkx9bGH5nq3s`
+const clientID = `?client_id=${REACT_APP_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+const  Commen=()=> {
   const [loading, setLoading] = useState(false);
-  const [dataa, setdataa] = useState([]);
-  const [input, setinput] = useState("");
-  const [search, setsearch] = useState("dog");
-  const Apical = async () => {
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
+  // const [pagination, setpagination]=useState()
+ 
+  // const handelchange=(event,pags)=>{
+  //   setpagination(pages)
+  // }
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  const fetchImages = async () => {
     setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
     try {
-      const res = await axios.get(
-        `https://api.unsplash.com/search/photos?page=1&query=${search}&client_id=fNZxU3j0BwP87u2Fco6aFSZOkn-4S9kmkx9bGH5nq3s`
-      );
-      setdataa(res.data.results);
+      const response = await fetch(url);
+      const data = await response.json();
+      
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+        
+      });
       setLoading(false);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
     }
   };
   useEffect(() => {
-    Apical();
-  }, [search]);
-  const sumbit = (e) => {
+    const event = window.addEventListener("scroll", () => {
+      if (
+        (!loading && window.innerHeight + window.scrollY) >=
+        document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        });
+      }
+    });
+
+    return () => window.removeEventListener("scroll", event);
+  }, []);
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setsearch(input);
+    setPage(1);
   };
+
   return (
     <div className="container">
-      <form
-      onSubmit={sumbit}
-        className="example"
-        style={{ margin: "auto", maxWidth: "300px", marginTop: "30px" }}
-      >
-        <input
-          className="input"
-          type="text"
-          value={input}
-          onChange={(e) => setinput(e.target.value)}
-          required
-        />
-        <button type="submit">
-          <i class="fa fa-search"></i>
-        </button>
-      </form>
-      <h1>Results for {search || "dog"}</h1>
-      <div>
-        {loading ? (
-          <div class="spinner-border text-primary" role="status">
-            <span class="visually-hidden">Loading...</span>
-          </div>
-        ) : (
-          <div className="row">
-            {dataa.map((cur, index) => {
-              return   <Images img={cur.urls.small_s3} />;
-            })}
-          </div>
+      <form onSubmit={handleSubmit}
+  className="example"
+  style={{ margin: "auto", maxWidth: "300px", marginTop: "30px" }}>
+  <input
+    className="input"
+    type="text"
+    value={query}
+    onChange={(e) => setQuery(e.target.value)}  
+      required
+  />
+  <button type="sumbit">
+    <i className="fa fa-search"></i>
+  </button>
+</form>
+
+        {
+          loading ? (
+          <div className="text-center">
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>)
+          :(
+      <div className="row">
+         {photos.map((image, index) => {
+         return(
+          <Images key={index} img={image.urls.regular}/>
+         )
+         }
+          
         )}
       </div>
+      
+       ) }
+        {/* <div style={{ display: 'flex', padding: 30,alignItems:"center",justifyContent:"center" }}> */}
+      {/* <h4>How to use Pagination Component in ReactJS?</h4> */}
+      {/* <Pagination count={counts}  /> */}
+{/*      
+  <Pagination count={5} onChange={handelchange}/>
+    </div> */}
     </div>
   );
-};
+}
 
 export default Commen;
--- src/component/Images.js
+++ src/component/Images.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Images(props) {
+const Images=(props)=> {
   return (
         
         <div className='col-md-4 mt-3 col-lg-4'>
-         <img class="img-fluid" src={props.img} alt="image" style={{width:"300px",height:"200px",objectFit: 'cover'}} />
+         <img className="img-fluid" src={props.img} alt="image" style={{width:"300px",height:"200px",objectFit: 'cover',marginBottom:"15px"}} />
          </div>
   )
 }
